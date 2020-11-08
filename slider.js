5ec27ef33913b443f3c77345e281e33b1a2e43b4
user <user@iMac-user.local>
2020-11-08 18:37:43 +0300

    Change slider method

--- slider.js
+++ slider.js
@@ -1,19 +1,26 @@
 const pictures = ['l_1.jpg', 's_4.jpg', 's_5.jpg', 'l_1.jpg', 'l_7.jpg', 'l_2.jpg', 'l_6.jpg', 'l_4.jpg', 'l_2.jpg', 'l_5.jpg', 's_1.jpg', 'l_3.jpg', 'l_8.jpg'];
 const pathToPicture = './img/';
-let next = 0; 
+let activeSlide = 0;
+const slider1 = document.getElementById('slider-1');
 
-const right = () => { 
-const obj = document.getElementById("img");
-  if (next < pictures.length - 1)  next++ 
-   else
-     next = 0;
-     obj.src = pathToPicture + pictures[next];	 
+const addSlider = (activeSlide) => {
+  pictures.map((pic, indx) => {
+    const slide = document.createElement("img");
+    slide.style.display = indx === activeSlide ? "block" : "none"; //видна только 
+    slide.style.width = '100%';
+    slide.src = pathToPicture + pic;
+
+    slider1.appendChild(slide);
+  })
 }
 
-const left = () => {     
-const obj = document.getElementById("img");
-if (next > 0) next--;
-  else
-    next = pictures.length - 1;
-    obj.src = pathToPicture + pictures[next];	  			 
+const sl1Left = () => {
+  activeSlide--;
+  
 }
+
+slider1.style.border = "thin dotted red"; //border чтобы видеть границы div
+addSlider(activeSlide); // в контейнер slider1 добавляем кадры сладов
+
+
+
