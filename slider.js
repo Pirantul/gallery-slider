3b4637e5467c8eda41cb0534e14dc5e320202027
user <user@iMac-user.local>
2020-11-10 17:28:16 +0300

    added random render images

--- slider.js
+++ slider.js
@@ -7,17 +7,15 @@ const onClickSliderBtn = (el, direction) => {
 
 //constructor
 class Slider {
-  shift = 1; //slider shift offset
+  shift = 1;                                                //slider shift offset
   pathToPicture = "img/";
   constructor(tagId) {
-    this.step = 0; //number of start slide
+    this.step = 0;                                          //number of start slide
     this.tagId = tagId;
   }
   clickBtn(direction) {
     const slider = document.getElementById(this.tagId);
-    const imagesCount = slider.childElementCount - 2;
-    const lastSlide = document.querySelector('img[class='+ this.tagId + '][style*="display: block"]'); 
-    lastSlide.style.display = "none"; //убираем предыдущий слайд
+    const imagesCount = slider.childElementCount - 2;       // count of slides, 2 - buttons
     
     if (direction === 'inc') {
       this.step += 1;
@@ -26,23 +24,24 @@ class Slider {
       this.step -= 1;
       if (this.step < 0) this.step = imagesCount - 3;
     }
-    slider.children[this.step].style.display = "block";;
+    this.renderSlide();
+  }
+  setStep(step) {
+    this.step = +step;
+    this.renderSlide();
+  }
+  renderSlide() {
+    const lastSlide = document.querySelector(`img[class=${this.tagId}][style*="display: block"]`); 
+    lastSlide.style.display = "none";                       //delete last slide
+    const slider = document.getElementById(this.tagId);
+    slider.children[this.step].style.display = "block";;    //render new slide
   }
 }
 
-//create a slider
+//create a slider1
 const slider1 = new Slider('slider1');
-// const slider2 = sliderFactory.makeSlider('slider2');
-
-//change the default slider2 properties
-// slider2.shift = 2;
-// slider2.step = 3;
-
-// slider1.addButton("img/left.png", 'dec', '40px');
-// slider1.addButton("img/right.png", 'inc', '40px');
-// slider2.addButton("img/left.png", 'dec', '40px');
-// slider2.addButton("img/right.png", 'inc', '40px');
-
-// slider1.step = 5;
+slider1.setStep(5);
 
 // setTimeout(()=>slider1.step=3, 1000)
+setInterval(()=>slider1.setStep(Math.floor(Math.random() * Math.floor(7))), 1000);
+
